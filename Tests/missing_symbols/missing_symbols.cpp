ae331f334a3a853ba23f6911ff3466dd23d2636f
Stefan <47391841+ostef@users.noreply.github.com>
2023-11-22 17:08:28 +0100

    Testing first drafts

---
+++ Tests/missing_symbols/missing_symbols.cpp
@@ -0,0 +1,14 @@
+#include <Windows.h>
+#include <d3d11.h>
+
+int main ()
+{
+    BOOL enabled;
+    AreShortNamesEnabled (NULL, &enabled);
+
+    HDC foo;
+    UINT_PTR bar;
+    INT_PTR bar;
+
+    ID3D11DeviceChild d;
+}
